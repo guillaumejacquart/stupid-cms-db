643f5b5c9b4c13f08a40ce11cf813e6ef2f69abf
Guillaume Jacquart <Guillaume@euphor.fr>
2016-08-29 16:50:40 +0200

    Code refactoring on server side. Enable editor adding and removing using toolbar

--- cms.js
+++ cms.js
@@ -16,83 +16,18 @@ module.exports = function(options, app) {
 	app.set('view engine', 'mustache');
 	app.set('views', path.join(__dirname, 'views'));
 	
-	var db = new Datastore({ filename: options.dbPath, autoload: true });
-		
-	var router = express.Router();
-	router.get('/:page?', function (req, res, next) {
-		var page = req.params.page;
-		if(!page && options.index){
-			page = options.index;
-		}
-		
-		console.log('requesting page : ' + page);
-		if(!page.endsWith('.html')){
-			next();
-		}
-		
-		var filepath = path.join(options.sitePath, page);
+	var db = new Datastore({ filename: options.dbPath || 'db.data', autoload: true });
+	options.db = db;
 	
-		fs.access(filepath, fs.F_OK, function(){
-			if(req.body){
-				fs.readFile(filepath, 'utf8', function(err, data){
-					if(!data){
-						next();
-						return;
-					}
-					
-					$ = cheerio.load(data);
-					
-					db.find({page: page}, function (err, docs) {
-						docs.forEach(function(d){
-							elem = $('[data-content="' + d.name + '"]');
-							if(elem.attr('data-repeatable') == "true"){
-								if(!d.repeats){
-									return;
-								}
-								
-								d.repeats.sort(function(a, b){
-									return a.index === b.index ? 0 : a.index < b.index ? -1 : 1;
-								}).forEach(function(c) {										
-									if(c.index === 0){
-										elem.html(c.html);
-									} else {
-										var newEl = elem.clone().html(c.html);
-										var alreadyCreated = elem.siblings('[data-repeatable]');
-										if(alreadyCreated.length){
-											alreadyCreated.last().after(newEl);
-										} else {
-											elem.after(newEl);
-										}
-									}
-								});
-							} else {
-								elem.html(d.html);
-								if(d.attrs){
-									d.attrs.forEach(function(a){
-										elem.attr(a.name, a.value);
-									});
-								}
-							}
-						});
-						
-						// Add minimal css and js to html.
-						$('body').append('<script src="/cms/js/editable.js"></script>');
-						
-						res.status(200).send($.html());
-					});
-				});
-			}			
-		})	
-	});
+	var pageLoader = require('./lib/page_loader')(options);	
 	
-	app.use(router);
+	app.use(pageLoader);
 	
 	app.use(express.static(options.sitePath, {
 		extensions: [],
 		index: false
 	}));
 	
-	options.db = db;
-	var routes = require('./routes')(options);
+	var routes = require('./lib/editor')(options);
 	app.use('/cms', routes);
 };
--- public/js/editable.js
+++ public/js/editable.js
@@ -1,261 +1,353 @@
-$(document).ready(function(){
+(function () {
+	$(document).ready(function(){
 	
-	$.get('/cms/user', function(response) {
-		if(response){	
-			
-			$('head').append('<link href="/cms/css/font-awesome.min.css" type="text/css" rel="stylesheet" />');
-			$('head').append('<link href="/cms/css/main.css" type="text/css" rel="stylesheet" />');
-			$('body')
-				.append('<div class="cms-notification"></div>');
-			
-			$.get('/cms/edition', function(response) {
-				if(response){
-					$('body').prepend(response);
-				}
-			});
-			
-			$('.cms-admin-handle').click(function(){
-				$('.cms-admin').toggleClass('extended');
-				$(this).text($('.cms-admin').hasClass('extended') ? '>' : '<');
-			});
-			
-			var notif = $('.cms-notification');
-			
-			$( window ).resize(function() {
-				initRepeatable();
-			});
-			
-			var script = document.createElement('script');
-			script.src = "/cms/js/tinymce/tinymce.min.js";
-			script.onload = function () {
-				initEditor();
+		$.get('/cms/user', function(response) {
+			if(response){	
 				
-				$('body').on('click', '[data-content]', function(){
-						if(!$(this).is('img')){
-							return;
-						}
-					})
+				$('head').append('<link href="/cms/css/font-awesome.min.css" type="text/css" rel="stylesheet" />');
+				$('head').append('<link href="/cms/css/main.css" type="text/css" rel="stylesheet" />');
 				
-					.on('blur', '[data-content]', function(){
-						var elem = this;
-						var attrs = getAttributes($(elem));
-											
-						data = {
-							name: $(elem).attr('data-content'), 
-							attrs: attrs
-						};
-						
-						var html = $(elem).html();
-						if($(elem).attr('data-repeatable') == "true"){
-							data.repeatIndex = $(elem).index('[data-content=' + data.name + ']');
-							data.repeatHtml = html;
-						} else {							
-							data.innerHtml = html;
-						}
-						
-						$.ajax({
-							url:'/cms/edit',
-							type:"POST",
-							data: JSON.stringify(data),
-							contentType:"application/json; charset=utf-8",
-							dataType:"json",
-							success: function(){
-								notif.html('Content saved !').fadeIn(500, function(){
-									window.setTimeout(function(){
-										notif.fadeOut(500);
-									}, 2000);
-								});
-							}
-						});
-					});
-			};
-			document.head.appendChild(script);		
-		}
-		
-		function initEditor(){		
-			initTinymce();
-			initRepeatable();
-			initCreateEditable();
-			
-			$('body').on('click', '.cms-repeat-actions .cms-copy', function(){
-				var elem = $('[data-content][data-repeatable]').eq($(this).closest('.cms-repeat-actions').index('index'));
-				var newElem = elem.clone();
-				$.each(["id", "contenteditable"],function(i,attrName){
-					newElem.removeAttr(attrName);
+				var cms = $('<div class="stupid-cms"></div>');
+				$('body').append(cms);
+				
+				cms.append('<div class="cms-notification"></div>');
+				
+				$.get('/cms/edition', function(response) {
+					if(response){
+						cms.append(response);
+					}
 				});
-				newElem.removeClass('mce-content-body mce-edit-focus');
-				elem.after(newElem);
-				initRepeatable();
-				initTinymce(newElem);
-			});
+				
+				$('.cms-admin-handle').click(function(){
+					$('.cms-admin').toggleClass('extended');
+					$(this).text($('.cms-admin').hasClass('extended') ? '>' : '<');
+				});
+				
+				var notif = $('.cms-notification');
+				
+				$(window).resize(function() {
+					initRepeatable();
+					initCreateEditable();
+				});
+				
+				var script = document.createElement('script');
+				script.src = "/cms/js/tinymce/tinymce.min.js";
+				script.onload = function () {
+					initEditor();
+					
+					$('body')				
+						.on('blur', '[data-content]', function(){
+							var elem = this;
+							var attrs = getAttributes($(elem));
+												
+							data = {
+								name: $(elem).attr('data-content'), 
+								attrs: attrs
+							};
+							
+							var html = $(elem).html();
+							if($(elem).attr('data-repeatable') == "true"){
+								data.repeatIndex = $(elem).index('[data-content=' + data.name + ']');
+								data.innerHtml = html;
+							} else {							
+								data.innerHtml = html;
+							}
+							
+							$.ajax({
+								url:'/cms/edit',
+								type:"POST",
+								data: JSON.stringify(data),
+								contentType:"application/json; charset=utf-8",
+								dataType:"json",
+								success: function(){
+									notif.html('Content saved !').fadeIn(500, function(){
+										window.setTimeout(function(){
+											notif.fadeOut(500);
+										}, 2000);
+									});
+								}
+							});
+						});
+				};
+				document.head.appendChild(script);		
+			}
 			
-			$('body').on('click', '.cms-repeat-actions .cms-delete', function(){
-				var actions = $(this).closest('.cms-repeat-actions');
+			function initEditor(){		
+				initTinymce();
+				initRepeatable();
+				initCreateEditable();
+				
+				cms.on('click', '.cms-repeat-actions .cms-copy', function(){
+					var elem = $('[data-content][data-repeatable]').eq($(this).closest('.cms-repeat-actions').index('index'));
+					var newElem = elem.clone();
+					$.each(["id", "contenteditable"],function(i,attrName){
+						newElem.removeAttr(attrName);
+					});
+					newElem.removeClass('mce-content-body mce-edit-focus');
+					elem.after(newElem);
+					initRepeatable();
+					initTinymce();
+				});
+				
+				cms.on('click', '.cms-repeat-actions .cms-delete', function(){
+					var actions = $(this).closest('.cms-repeat-actions');
+					var index = actions.data('index');
+					var name = actions.data('name');
+					
+					var elem = $('[data-content="' + name + '"][data-repeatable]').eq(index);
+					var data = JSON.stringify({
+						name: name,
+						repeatIndex: index
+					});
+					
+					$.ajax({
+						url:'/cms/remove',
+						type:"POST",
+						data: data,
+						contentType:"application/json; charset=utf-8",
+						dataType:"json",
+						success: function(){
+							elem.remove();
+							initRepeatable();
+							notif.html('Content deleted !').fadeIn(500, function(){
+								window.setTimeout(function(){
+									notif.fadeOut(500);
+								}, 2000);
+							});
+						}
+					});
+				});
+				
+				cms.on('click', '.cms-repeat-actions .cms-move-up', function(){
+					moveRepeatable('up', this);
+				});
+				
+				cms.on('click', '.cms-repeat-actions .cms-move-down', function(){
+					moveRepeatable('down', this);
+				});
+			}
+			
+			function initRepeatable(){	
+				$('.cms-repeat-actions').remove();		
+				$('[data-content][data-repeatable]').each(function(){
+					var elem = $(this);
+					var name = elem.attr('data-content');
+					var index = elem.index('[data-content="' + name + '"][data-repeatable]');
+					
+					var handle = $('<div class="cms-repeat-actions"><div class="cms-copy"><span class="fa fa-clone"></span></div>' + 
+						'<div class="cms-delete"><span class="fa fa-trash-o"></div>' + 
+						'<div class="cms-move-up"><span class="fa fa-angle-up"></div>' + 
+						'<div class="cms-move-down"><span class="fa fa-angle-down"></div></div>');
+						
+					handle.css({
+						top: elem.offset().top,
+						left: elem.width() + elem.offset().left - 100,
+					});
+					
+					handle.data('index', index);
+					handle.data('name', name);
+					cms.append(handle);
+				});
+			}
+			
+			function moveRepeatable(dir, elemBtn){
+				var actions = $(elemBtn).closest('.cms-repeat-actions');
 				var index = actions.data('index');
 				var name = actions.data('name');
 				
+				var length = $('[data-content="' + name + '"][data-repeatable]').length;
 				var elem = $('[data-content="' + name + '"][data-repeatable]').eq(index);
 				var data = JSON.stringify({
 					name: name,
-					repeatIndex: index
+					repeatIndex: index,
+					newRepeatIndex: dir === 'up' ? Math.max(index - 1, 0) : Math.min(index + 1, length - 1)
 				});
 				
 				$.ajax({
-					url:'/cms/remove',
+					url:'/cms/order',
 					type:"POST",
 					data: data,
 					contentType:"application/json; charset=utf-8",
 					dataType:"json",
 					success: function(){
-						elem.remove();
+						if(dir === 'up'){
+							$(elem).after($(elem).prev());
+						} else {						
+							$(elem).before($(elem).next());
+						}						
 						initRepeatable();
-						notif.html('Content deleted !').fadeIn(500, function(){
+						notif.html('Content moved !').fadeIn(500, function(){
 							window.setTimeout(function(){
 								notif.fadeOut(500);
 							}, 2000);
 						});
 					}
 				});
-			});
+			}
 			
-			$('body').on('click', '.cms-repeat-actions .cms-move-up', function(){
-				moveRepeatable('up', this);
-			});
-			
-			$('body').on('click', '.cms-repeat-actions .cms-move-down', function(){
-				moveRepeatable('down', this);
-			});
-		}
-		
-		function initRepeatable(){	
-			$('.cms-repeat-actions').remove();		
-			$('[data-content][data-repeatable]').each(function(){
-				var elem = $(this);
-				var name = elem.attr('data-content');
-				var index = elem.index('[data-content="' + name + '"][data-repeatable]');
-				
-				var handle = $('<div class="cms-repeat-actions"><div class="cms-copy"><span class="fa fa-clone"></span></div>' + 
-					'<div class="cms-delete"><span class="fa fa-trash-o"></div>' + 
-					'<div class="cms-move-up"><span class="fa fa-angle-up"></div>' + 
-					'<div class="cms-move-down"><span class="fa fa-angle-down"></div></div>');
-					
-				handle.css({
-					top: elem.offset().top,
-					left: elem.width() + elem.offset().left - 100,
-				});
-				
-				handle.data('index', index);
-				handle.data('name', name);
-				$('body').append(handle);
-			});
-		}
-		
-		function moveRepeatable(dir, elemBtn){
-			var actions = $(elemBtn).closest('.cms-repeat-actions');
-			var index = actions.data('index');
-			var name = actions.data('name');
-			
-			var length = $('[data-content="' + name + '"][data-repeatable]').length;
-			var elem = $('[data-content="' + name + '"][data-repeatable]').eq(index);
-			var data = JSON.stringify({
-				name: name,
-				repeatIndex: index,
-				newRepeatIndex: dir === 'up' ? Math.max(index - 1, 0) : Math.min(index + 1, length - 1)
-			});
-			
-			$.ajax({
-				url:'/cms/order',
-				type:"POST",
-				data: data,
-				contentType:"application/json; charset=utf-8",
-				dataType:"json",
-				success: function(){
-					if(dir === 'up'){
-						$(elem).after($(elem).prev());
-					} else {						
-						$(elem).before($(elem).next());
-					}						
-					initRepeatable();
-					notif.html('Content moved !').fadeIn(500, function(){
-						window.setTimeout(function(){
-							notif.fadeOut(500);
-						}, 2000);
-					});
-				}
-			});
-		}
-		
-		function initTinymce(){			
-			tinymce.remove('[data-content]');
-			tinymce.init({
-				selector: '[data-content]',
-				inline: true,
-				toolbar: 'undo redo image link paste | bold italic underline',
-				menubar: false,
-				plugins: "image link paste visualblocks",
-				paste_as_text: true,
-				forced_root_block : "",
-				visualblocks_default_state: true
-			});
-		}
-		
-		function initCreateEditable(){
-			$('body *').filter(function(){
-				return !$(this).closest('[data-content]').length && !$(this).find('[data-content]').length;
-			})
-			.hover(function(){
-				$(this).css({ border: '1px dashed #ddd' });
-			}, function(){				
-				$(this).css({ border: 'initial' });
-			})
-			.dblclick(function(e){
-				e.stopPropagation();
-				var that = $(this);
-				console.log(that.getPath());
-			});
-		}
-		
-		function getAttributes(elem){	
-			var attributes = [];	
-			elem.each(function() {
-				$.each(this.attributes, function() {
-					// this.attributes is not a plain object, but an array
-					// of attribute nodes, which contain both the name and value
-					if(this.specified) {
-						attributes.push({name: this.name, value: this.value});
+			function initTinymce(){			
+				tinymce.remove();
+				tinymce.init({
+					selector: '[data-content]',
+					inline: true,
+					toolbar: 'undo redo image link paste | bold italic underline | remove-editor',
+					menubar: false,
+					plugins: "image link paste visualblocks",
+					paste_as_text: true,
+					forced_root_block : "",
+					visualblocks_default_state: true,
+					setup: function (editor) {
+						editor.addButton('remove-editor', {
+							text: 'Remove editor',
+							icon: false,
+							onclick: function () {
+								destroyEditable($(editor.bodyElement));
+							}
+						});
 					}
 				});
-			});
-			return attributes;
+			}
+			
+			function initCreateEditable(){
+				$('body *').filter(function(){
+					return !$(this).closest('[data-content]').length 
+						&& !$(this).find('[data-content]').length
+						&& !$(this).closest('.stupid-cms').length;
+				})
+				.hover(function(){
+					var elem = $(this);
+					var path = elem.getPath();
+					
+					elem.css({ 
+						border: '1px dashed #ddd'
+					});
+					if($('.cms-new-content').filter(function(){
+						return $(this).data('path') === path;
+					}).length > 0){
+						return;
+					}
+					
+					var handle = $('<div class="cms-new-content">' + 
+						'<div class="cms-create"><span class="fa fa-plus"></span></div>' + 
+						'</div>');
+					
+					handle.css({
+						top: elem.offset().top,
+						left: elem.width() + elem.offset().left - 100,
+					});
+					
+					handle.data('path', path);
+					cms.append(handle);
+				}, function(e){	
+					var elem = $(this);	
+					var path = elem.getPath();
+					if(!$(e.relatedTarget).hasClass('cms-create')){
+						$('.cms-new-content').filter(function(){
+							return $(this).data('path') === path;
+						}).remove();
+						
+						elem.css({ 
+							border: 'initial' 
+						});
+					}
+				});
+				
+				cms.on('click', '.cms-create', function(){
+					var that = $(this).closest('.cms-new-content');
+					var path = that.data('path');
+					
+					var data = JSON.stringify({
+						selector: path
+					});
+					
+					$.ajax({
+						url:'/cms/editable',
+						type:"POST",
+						data: data,
+						contentType:"application/json; charset=utf-8",
+						dataType:"json",
+						success: function(response){
+							$(path).attr('data-content', response.name);
+							that.remove();
+							
+							notif.html('Content is now editable !').fadeIn(500, function(){
+								window.setTimeout(function(){
+									notif.fadeOut(500);
+								}, 2000);
+							});	
+							initTinymce();
+						}
+					});
+				})
+				.on('mouseleave', '.cms-create', function(){
+					$(this).closest('.cms-new-content').remove();
+				});
+			}
+			
+			function destroyEditable(elem){
+				
+				var id = elem.attr('data-content');
+				
+				$.ajax({
+					url:'/cms/editable/' + id,
+					type:"DELETE",
+					contentType:"application/json; charset=utf-8",
+					dataType:"json",
+					success: function(response){
+						elem.removeAttr('data-content');
+						
+						notif.html('Content not editable !').fadeIn(500, function(){
+							window.setTimeout(function(){
+								notif.fadeOut(500);
+							}, 2000);
+						});	
+						initTinymce();
+						initCreateEditable();
+					}
+				});
+			}
+			
+			function getAttributes(elem){	
+				var attributes = [];	
+				elem.each(function() {
+					$.each(this.attributes, function() {
+						// this.attributes is not a plain object, but an array
+						// of attribute nodes, which contain both the name and value
+						if(this.specified) {
+							attributes.push({name: this.name, value: this.value});
+						}
+					});
+				});
+				return attributes;
+			}
+
+		});
+	});
+
+	jQuery.fn.extend({
+		getPath: function () {
+			var path, node = this;
+			while (node.length) {
+				var realNode = node[0], name = realNode.localName;
+				if (!name) break;
+				name = name.toLowerCase();
+
+				var parent = node.parent();
+
+				var sameTagSiblings = parent.children(name);
+				if (sameTagSiblings.length > 1) { 
+					allSiblings = parent.children();
+					var index = allSiblings.index(realNode) + 1;
+					if (index > 1) {
+						name += ':nth-child(' + index + ')';
+					}
+				}
+
+				path = name + (path ? '>' + path : '');
+				node = parent;
+			}
+
+			return path;
 		}
-
-	});	
-		
-});
-
-jQuery.fn.extend({
-    getPath: function () {
-        var path, node = this;
-        while (node.length) {
-            var realNode = node[0], name = realNode.localName;
-            if (!name) break;
-            name = name.toLowerCase();
-
-            var parent = node.parent();
-
-            var sameTagSiblings = parent.children(name);
-            if (sameTagSiblings.length > 1) { 
-                allSiblings = parent.children();
-                var index = allSiblings.index(realNode) + 1;
-                if (index > 1) {
-                    name += ':nth-child(' + index + ')';
-                }
-            }
-
-            path = name + (path ? '>' + path : '');
-            node = parent;
-        }
-
-        return path;
-    }
-});
+	});
+})();
