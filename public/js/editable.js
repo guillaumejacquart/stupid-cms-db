4c1dc867120fd60450d4616829b91dad41a8a493
Guillaume Jacquart <Guillaume@euphor.fr>
2016-08-31 17:22:32 +0200

    QC on editable.js

--- public/js/editable.js
+++ public/js/editable.js
@@ -1,7 +1,7 @@
 (function () {
 	$(document).ready(function(){
 	
-		$.get('/cms/user', function(response) {			
+		$.get("/cms/user", function(response) {			
 			var cms, 
 				notif;
 			
@@ -20,68 +20,68 @@
 			}
 			
 			function initCreateEditable(){
-				$('div, ul, ol, h1, h2, h3, h4, h5, p').filter(function(){
-					return !$(this).closest('[data-content]').length 
-						&& !$(this).find('[data-content]').length
-						&& !$(this).closest('.stupid-cms').length;
+				$("div, ul, ol, h1, h2, h3, h4, h5, p").filter(function(){
+					return !$(this).closest("[data-content]").length 
+						&& !$(this).find("[data-content]").length
+						&& !$(this).closest(".stupid-cms").length;
 				})
 				.hover(function(){
 					var elem = $(this);
 					var path = elem.getPath();
 					
 					elem.css({ 
-						border: '1px dashed #ddd'
+						border: "1px dashed #ddd"
 					});
-					if($('.cms-new-content').filter(function(){
-						return $(this).data('path') === path;
+					if($(".cms-new-content").filter(function(){
+						return $(this).data("path") === path;
 					}).length > 0){
 						return;
 					}
 					
-					var handle = $('<div class="cms-new-content">' + 
-						'<div class="cms-create"><span class="fa fa-plus"></span></div>' + 
-						'</div>');
+					var handle = $("<div class=\"cms-new-content\">" + 
+						"<div class=\"cms-create\"><span class=\"fa fa-plus\"></span></div>" + 
+						"</div>");
 					
 					handle.css({
 						top: elem.offset().top,
 						left: elem.width() + elem.offset().left - 25,
 					});
 					
-					handle.data('path', path);
+					handle.data("path", path);
 					cms.append(handle);
 				}, function(e){	
 					var elem = $(this);	
 					var path = elem.getPath();
-					if(!$(e.relatedTarget).hasClass('cms-create')){
-						$('.cms-new-content').filter(function(){
-							return $(this).data('path') === path;
+					if(!$(e.relatedTarget).hasClass("cms-create")){
+						$(".cms-new-content").filter(function(){
+							return $(this).data("path") === path;
 						}).remove();
 						
 						elem.css({ 
-							border: 'initial' 
+							border: "initial"
 						});
 					}
 				});
 				
-				cms.on('click', '.cms-create', function(){
-					var that = $(this).closest('.cms-new-content');
-					var path = that.data('path');
+				cms.on("click", ".cms-create", function(){
+					var that = $(this).closest(".cms-new-content");
+					var path = that.data("path");
 					
 					var data = JSON.stringify({
 						selector: path
 					});
 					
 					$.ajax({
-						url:'/cms/editable',
+						url:"/cms/editable",
 						type:"POST",
 						data: data,
 						contentType:"application/json; charset=utf-8",
 						dataType:"json",
 						success: function(response){
-							$(path).attr('data-content', response.name);
+							$(path).attr("data-content", response.name);
 							that.remove();
 							
-							notif.html('Content is now editable !').fadeIn(500, function(){
+							notif.html("Content is now editable !").fadeIn(500, function(){
 								window.setTimeout(function(){
 									notif.fadeOut(500);
 								}, 2000);
@@ -90,24 +90,24 @@
 						}
 					});
 				})
-				.on('mouseleave', '.cms-create', function(){
-					$(this).closest('.cms-new-content').remove();
+				.on("mouseleave", ".cms-create", function(){
+					$(this).closest(".cms-new-content").remove();
 				});
 			}
 			
 			function destroyEditable(elem){
 				
-				var id = elem.attr('data-content');
+				var id = elem.attr("data-content");
 				
 				$.ajax({
-					url:'/cms/editable/' + id,
+					url:"/cms/editable/" + id,
 					type:"DELETE",
 					contentType:"application/json; charset=utf-8",
 					dataType:"json",
 					success: function(response){
-						elem.removeAttr('data-content');
+						elem.removeAttr("data-content");
 						
-						notif.html('Content not editable !').fadeIn(500, function(){
+						notif.html("Content not editable !").fadeIn(500, function(){
 							window.setTimeout(function(){
 								notif.fadeOut(500);
 							}, 2000);
@@ -119,55 +119,55 @@
 			}
 			
 			function initRepeatable(){	
-				$('.cms-repeat-actions').remove();		
-				$('[data-content][data-repeatable]').each(function(){
+				$(".cms-repeat-actions").remove();		
+				$("[data-content][data-repeatable]").each(function(){
 					var elem = $(this);
-					var name = elem.attr('data-content');
-					var index = elem.index('[data-content="' + name + '"][data-repeatable]');
+					var name = elem.attr("data-content");
+					var index = elem.index("[data-content=\"" + name + "\"][data-repeatable]");
 					
-					var handle = $('<div class="cms-repeat-actions"><div class="cms-copy"><span class="fa fa-clone"></span></div>' + 
-						'<div class="cms-delete"><span class="fa fa-trash-o"></div>' + 
-						'<div class="cms-move-up"><span class="fa fa-angle-up"></div>' + 
-						'<div class="cms-move-down"><span class="fa fa-angle-down"></div></div>');
+					var handle = $("<div class=\"cms-repeat-actions\"><div class=\"cms-copy\"><span class=\"fa fa-clone\"></span></div>" + 
+						"<div class=\"cms-delete\"><span class=\"fa fa-trash-o\"></div>" + 
+						"<div class=\"cms-move-up\"><span class=\"fa fa-angle-up\"></div>" + 
+						"<div class=\"cms-move-down\"><span class=\"fa fa-angle-down\"></div></div>");
 						
 					handle.css({
 						top: elem.offset().top,
 						left: elem.width() + elem.offset().left - 100,
 					});
 					
-					handle.data('index', index);
-					handle.data('name', name);
+					handle.data("index", index);
+					handle.data("name", name);
 					cms.append(handle);
 				});
 			}
 			
 			function moveRepeatable(dir, elemBtn){
-				var actions = $(elemBtn).closest('.cms-repeat-actions');
-				var index = actions.data('index');
-				var name = actions.data('name');
+				var actions = $(elemBtn).closest(".cms-repeat-actions");
+				var index = actions.data("index");
+				var name = actions.data("name");
 				
-				var length = $('[data-content="' + name + '"][data-repeatable]').length;
-				var elem = $('[data-content="' + name + '"][data-repeatable]').eq(index);
+				var length = $("[data-content=\"" + name + "\"][data-repeatable]").length;
+				var elem = $("[data-content=\"" + name + "\"][data-repeatable]").eq(index);
 				var data = JSON.stringify({
 					name: name,
 					repeatIndex: index,
-					newRepeatIndex: dir === 'up' ? Math.max(index - 1, 0) : Math.min(index + 1, length - 1)
+					newRepeatIndex: dir === "up" ? Math.max(index - 1, 0) : Math.min(index + 1, length - 1)
 				});
 				
 				$.ajax({
-					url:'/cms/order',
+					url:"/cms/order",
 					type:"POST",
 					data: data,
 					contentType:"application/json; charset=utf-8",
 					dataType:"json",
 					success: function(){
-						if(dir === 'up'){
+						if(dir === "up"){
 							$(elem).after($(elem).prev());
 						} else {						
 							$(elem).before($(elem).next());
 						}						
 						initRepeatable();
-						notif.html('Content moved !').fadeIn(500, function(){
+						notif.html("Content moved !").fadeIn(500, function(){
 							window.setTimeout(function(){
 								notif.fadeOut(500);
 							}, 2000);
@@ -179,17 +179,17 @@
 			function initTinymce(){			
 				tinymce.remove();
 				tinymce.init({
-					selector: '[data-content]',
+					selector: "[data-content]",
 					inline: true,
-					toolbar: 'undo redo image link paste | bold italic underline | remove-editor',
+					toolbar: "undo redo image link paste | bold italic underline | remove-editor",
 					menubar: false,
 					plugins: "image link paste visualblocks",
 					paste_as_text: true,
 					forced_root_block : "",
 					visualblocks_default_state: true,
 					setup: function (editor) {
-						editor.addButton('remove-editor', {
-							text: 'Remove editor',
+						editor.addButton("remove-editor", {
+							text: "Remove editor",
 							icon: false,
 							onclick: function () {
 								destroyEditable($(editor.bodyElement));
@@ -204,31 +204,31 @@
 				initRepeatable();
 				initCreateEditable();
 				
-				cms.on('click', '.cms-repeat-actions .cms-copy', function(){
-					var elem = $('[data-content][data-repeatable]').eq($(this).closest('.cms-repeat-actions').index('index'));
+				cms.on("click", ".cms-repeat-actions .cms-copy", function(){
+					var elem = $("[data-content][data-repeatable]").eq($(this).closest(".cms-repeat-actions").index("index"));
 					var newElem = elem.clone();
 					$.each(["id", "contenteditable"],function(i,attrName){
 						newElem.removeAttr(attrName);
 					});
-					newElem.removeClass('mce-content-body mce-edit-focus');
+					newElem.removeClass("mce-content-body mce-edit-focus");
 					elem.after(newElem);
 					initRepeatable();
 					initTinymce();
 				});
 				
-				cms.on('click', '.cms-repeat-actions .cms-delete', function(){
-					var actions = $(this).closest('.cms-repeat-actions');
-					var index = actions.data('index');
-					var name = actions.data('name');
+				cms.on("click", ".cms-repeat-actions .cms-delete", function(){
+					var actions = $(this).closest(".cms-repeat-actions");
+					var index = actions.data("index");
+					var name = actions.data("name");
 					
-					var elem = $('[data-content="' + name + '"][data-repeatable]').eq(index);
+					var elem = $("[data-content=\"" + name + "\"][data-repeatable]").eq(index);
 					var data = JSON.stringify({
 						name: name,
 						repeatIndex: index
 					});
 					
 					$.ajax({
-						url:'/cms/remove',
+						url:"/cms/remove",
 						type:"POST",
 						data: data,
 						contentType:"application/json; charset=utf-8",
@@ -236,7 +236,7 @@
 						success: function(){
 							elem.remove();
 							initRepeatable();
-							notif.html('Content deleted !').fadeIn(500, function(){
+							notif.html("Content deleted !").fadeIn(500, function(){
 								window.setTimeout(function(){
 									notif.fadeOut(500);
 								}, 2000);
@@ -245,48 +245,48 @@
 					});
 				});
 				
-				cms.on('click', '.cms-repeat-actions .cms-move-up', function(){
-					moveRepeatable('up', this);
+				cms.on("click", ".cms-repeat-actions .cms-move-up", function(){
+					moveRepeatable("up", this);
 				});
 				
-				cms.on('click', '.cms-repeat-actions .cms-move-down', function(){
-					moveRepeatable('down', this);
+				cms.on("click", ".cms-repeat-actions .cms-move-down", function(){
+					moveRepeatable("down", this);
 				});
 			}			
 			
 			if(response){	
 				
-				$('head').append('<link href="/cms/css/font-awesome.min.css" type="text/css" rel="stylesheet" />');
-				$('head').append('<link href="/cms/css/main.css" type="text/css" rel="stylesheet" />');
+				$("head").append("<link href=\"/cms/css/font-awesome.min.css\" type=\"text/css\" rel=\"stylesheet\" />");
+				$("head").append("<link href=\"/cms/css/main.css\" type=\"text/css\" rel=\"stylesheet\" />");
 				
-				cms = $('<div class="stupid-cms"></div>');
-				notif = $('<div class="cms-notification"></div>');
+				cms = $("<div class=\"stupid-cms\"></div>");
+				notif = $("<div class=\"cms-notification\"></div>");
 				cms.append(notif);
 				
-				$('body').append(cms);
+				$("body").append(cms);
 	
 				$(document).ajaxError(function(event, request, settings) {
 					console.log(event);
 					notif.html(request.responseJSON.message)
-						.addClass('error')
+						.addClass("error")
 						.fadeIn(500, function(){
 							window.setTimeout(function(){
 								notif.fadeOut(500, function(){
-									notif.removeClass('error')
+									notif.removeClass("error")
 								});
 							}, 2000);
 					});
 				});
 				
-				$.get('/cms/edition', function(response) {
+				$.get("/cms/edition", function(response) {
 					if(response){
 						cms.append(response);
 					}
 				});
 				
-				$('.cms-admin-handle').click(function(){
-					$('.cms-admin').toggleClass('extended');
-					$(this).text($('.cms-admin').hasClass('extended') ? '>' : '<');
+				$(".cms-admin-handle").click(function(){
+					$(".cms-admin").toggleClass("extended");
+					$(this).text($(".cms-admin").hasClass("extended") ? '>' : "<");
 				});
 				
 				
@@ -295,37 +295,37 @@
 					initCreateEditable();
 				});
 				
-				var script = document.createElement('script');
+				var script = document.createElement("script");
 				script.src = "/cms/js/tinymce/tinymce.min.js";
 				script.onload = function () {
 					initEditor();
 					
-					$('body')				
-						.on('blur', '[data-content]', function(){
+					$("body")				
+						.on("blur", "[data-content]", function(){
 							var elem = this;
 							var attrs = getAttributes($(elem));
 												
-							data = {
-								name: $(elem).attr('data-content'), 
+							var data = {
+								name: $(elem).attr("data-content"), 
 								attrs: attrs
 							};
 							
 							var html = $(elem).html();
-							if($(elem).attr('data-repeatable') === "true"){
-								data.repeatIndex = $(elem).index('[data-content=' + data.name + ']');
+							if($(elem).attr("data-repeatable") === "true"){
+								data.repeatIndex = $(elem).index("[data-content=' + data.name + ']");
 								data.innerHtml = html;
 							} else {							
 								data.innerHtml = html;
 							}
 							
 							$.ajax({
-								url:'/cms/edit',
+								url:"/cms/edit",
 								type:"POST",
 								data: JSON.stringify(data),
 								contentType:"application/json; charset=utf-8",
 								dataType:"json",
 								success: function(){
-									notif.html('Content saved !').fadeIn(500, function(){
+									notif.html("Content saved !").fadeIn(500, function(){
 										window.setTimeout(function(){
 											notif.fadeOut(500);
 										}, 2000);
@@ -354,15 +354,15 @@
 					allSiblings = parent.children();
 					var index = allSiblings.index(realNode) + 1;
 					if (index > 1) {
-						name += ':nth-child(' + index + ')';
+						name += ":nth-child(" + index + ")";
 					}
 				}
 
-				path = name + (path ? '>' + path : '');
+				path = name + (path ? ">" + path : "");
 				node = parent;
 			}
 
 			return path;
 		}
 	});
-})();
+}());
