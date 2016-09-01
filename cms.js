8d466b4fabb8f6c93150f45efc48641a0f85ad0a
Guillaume Jacquart <Guillaume@euphor.fr>
2016-09-01 17:27:43 +0200

    QC

--- cms.js
+++ cms.js
@@ -1,25 +1,25 @@
-var express = require('express');
+var express = require("express");
 var path = require("path");
 var fs = require("fs.extra");
-var cheerio = require('cheerio');
-var mustacheExpress = require('mustache-express');
+var cheerio = require("cheerio");
+var mustacheExpress = require("mustache-express");
 
-var Datastore = require('nedb');
+var Datastore = require("nedb");
 
 module.exports = function(options, app) {
 	
-	app.use('/cms', express.static(path.join(__dirname, 'public')));	
+	app.use("/cms", express.static(path.join(__dirname, "public")));	
 
-	// Register '.mustache' extension with The Mustache Express
-	app.engine('html', mustacheExpress());
+	// Register ".mustache" extension with The Mustache Express
+	app.engine("html", mustacheExpress());
 
-	app.set('view engine', 'mustache');
-	app.set('views', path.join(__dirname, 'views'));
+	app.set("view engine", "mustache");
+	app.set("views", path.join(__dirname, "views"));
 	
-	var db = new Datastore({ filename: options.dbPath || 'db.data', autoload: true });
+	var db = new Datastore({ filename: options.dbPath || "db.data", autoload: true });
 	options.db = db;
 	
-	var pageLoader = require('./lib/page_loader')(options);	
+	var pageLoader = require("./lib/page_loader")(options);	
 	
 	app.use(pageLoader);
 	
@@ -28,6 +28,6 @@ module.exports = function(options, app) {
 		index: false
 	}));
 	
-	var routes = require('./lib/editor')(options);
-	app.use('/cms', routes);
+	var routes = require("./lib/editor")(options);
+	app.use("/cms", routes);
 };
