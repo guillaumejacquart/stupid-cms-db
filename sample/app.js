8571fd8c4ddeabb33427be26a4dedcb1e9235d05
Guillaume Jacquart <Guillaume@euphor.fr>
2016-09-22 18:17:06 +0200

    removes useless dependencies and files.

--- sample/app.js
+++ sample/app.js
@@ -1,53 +1,13 @@
 var express = require("express");
 var path = require("path");
-var favicon = require("serve-favicon");
-var logger = require("morgan");
-var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
 var cms = require("../cms");
 
 var app = express();
 
-app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
 var sitePath = path.join(__dirname, "site");
 cms({
 	sitePath,
-	index: "index.html",
-	auth: {
-		type: "basic",
-		username: "login",
-		password: "password"
-	}
+	index: "index.html"
 }, app);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error("Not Found");
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get("env") === "development") {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-	next(err);
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  next(err);
-});
-
 
 module.exports = app;
