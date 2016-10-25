fe0b422e3ce955a42cfd89001b01f7e5a4d0d503
Guillaume Jacquart <guillaume.jacquart@live.fr>
2016-10-25 09:06:50 +0200

    Add dist to git (for npm).

---
+++ dist/server/managers/data_manager.js
@@ -0,0 +1,128 @@
+var fs = require("fs-extra");
+var cheerio = require("cheerio");
+var uuid = require("node-uuid");
+
+var dataDb;
+
+function editPageContents(contents, page, callback){	
+	var content = {
+		lastUpdatedAt: new Date(),
+		contents: contents
+	};
+	
+	dataDb.update({ page: page }, { $set: content }, { upsert: true }, function (err, numReplaced) {
+		callback(err);
+	});
+};
+
+function getPage(page, callback){
+	dataDb.find({ page: page }, function (err, docs) {	
+		if(!docs.length){
+			callback({
+				message: "Page does not exist"
+			});
+			return;
+		}
+		if(err){
+			throw err;
+		}
+		
+		callback(docs[0]);
+	});
+}
+
+function getMetadata(page, callback){
+	dataDb.find({ page: page }, function (err, docs) {	
+		if(!docs.length || !docs[0].metadata){
+			callback({
+				message: "Metadata does not exist"
+			});
+			return;
+		}
+		if(err){
+			throw err;
+		}
+		
+		callback(docs[0].metadata);
+	});
+}
+
+function saveMetadata(metadata, page, callback){	
+	dataDb.update({ page: page }, { $set: { metadata: metadata } }, { upsert: true }, function (err, numReplaced) {	
+		callback(err);
+	});
+};
+
+/* Change page html to make dom element editable */
+function makeElemEditable(selector, filepath, callback){
+	fs.access(filepath, fs.F_OK, function(){
+		fs.readFile(filepath, "utf8", function(err, data){
+			if(!data){
+				throw new Error("File does not exist");
+			}
+			
+			$ = cheerio.load(data);
+			
+			var elem = $(selector);
+			if(elem.length === 1){
+				var name = uuid.v1();
+				elem.attr("data-content", name);
+				fs.writeFile(filepath, $.html(), (err) => {
+					if (err){
+						throw err;
+					}
+					callback(null, name);
+				});
+			} else {
+				callback({
+					message: "The selector is incorrect: " + elem.length + " matches. You must set the editable content manually"
+				});
+			}
+		});	
+	});
+};
+
+/* Change page html to make dom element not editable */
+function destroyElemEditable(id, filepath, callback){
+	fs.access(filepath, fs.F_OK, function(){
+		fs.readFile(filepath, "utf8", function(err, data){
+			if(!data){
+				throw new Error("File does not exist");
+			}
+			
+			$ = cheerio.load(data);
+			
+			var elem = $("[data-content=\"" + id + "\"]");
+			if(elem.length === 1){
+				var name = uuid.v1();
+				elem.removeAttr("data-content");
+				fs.writeFile(filepath, $.html(), (err) => {
+					if (err) {
+						throw err;
+					}
+					dataDb.remove({ name: id }, {}, function (err, numRemoved) {
+						if (err) {
+							throw err;
+						}
+						callback();
+					});
+				});
+			} else {
+				throw new Error("Id matches " + elem.length + " elements");
+			}
+		});	
+	});
+};
+
+module.exports = function(dataDbArg){
+	dataDb = dataDbArg;
+	
+	return {
+		getPage,
+		editPageContents,
+		makeElemEditable,
+		destroyElemEditable,
+		getMetadata,
+		saveMetadata
+	};
+};
---
+++ dist/server/routes/cms.js
@@ -0,0 +1,192 @@
+var express = require("express");
+var fs = require("fs-extra");
+var cheerio = require("cheerio");
+var router = express.Router();
+var url = require("url");
+var path = require("path");
+var uuid = require("node-uuid");
+var exporter = require("../managers/export_manager");
+var multer = require("multer");
+var unzip = require("unzip");
+var uploadImage = multer({ dest: __dirname + "/../uploads-image/" });
+var uploadSite = multer({ dest: __dirname + "/../uploads-site/" });
+var passport = require("passport");
+
+var options,
+	dataManager,
+	userManager,
+	exportManager;
+
+var isAuthenticated = function(req, res, next){	
+	if (req.isAuthenticated())
+		return next();
+	
+	res.redirect(401, "/cms/login");
+}
+
+/* GET user infos. */
+router.get("/user", function(req, res, next) {	
+	if(!req.isAuthenticated()){
+		res.json(false);
+		return;
+	}
+	
+	var user = req.user;
+	var page = req.query.page;
+	
+	dataManager.getPage(req.query.page, function(data){	
+		user.lastUpdatedAt = data.lastUpdatedAt;
+		res.status(200).json(user);
+	});	
+});
+
+/* GET edition infos. */
+router.get("/edition", isAuthenticated, function(req, res, next) {
+	var page = req.query.page;
+	var filepath = path.join(options.sitePath, page);
+	fs.readdir(filepath, function(err, files){
+		res.render("cms-bar.html", { user: req.user });
+	});
+});
+
+/* GET page metadata infos. */
+router.get("/metadata", isAuthenticated, function(req, res, next) {	
+	dataManager.getMetadata(req.query.page, function(data){		
+		res.status(200).json(data);
+	});
+});
+
+/* POST page metadata infos. */
+router.post("/metadata", isAuthenticated, function(req, res, next) {	
+	var metadata = {
+		url: req.body.url,
+		title: req.body.title
+	}
+	
+	dataManager.saveMetadata(metadata, req.query.page, function(err){
+		if(err){
+			res.status(400).json(err);
+			return;
+		}
+		
+		res.json({status: "OK"});
+	});
+});
+
+/* POST edit the whole page contents. */
+router.post("/edit-page", isAuthenticated, function(req, res, next) {		
+	dataManager.editPageContents(req.body, req.query.page, function(err){
+		if(err) {
+			throw err;
+		}
+		exportManager.exportSite(path.join(__dirname, "../public"), function(folder){			
+			res.status(200).json({status: "OK"});
+		});
+	});
+});
+
+/* POST upload image. */
+router.post("/upload-image", uploadImage.single("cms_image_upload"), function(req, res, next) {		
+	res.json({
+		path: "uploads-image/" + req.file.filename
+	});
+});
+
+/* POST upload site files. */
+router.post("/upload-site", uploadSite.single("cms_site_upload"), function(req, res, next) {		
+	var file = req.file.path;
+	fs.emptyDir(options.sitePath, function (err) {
+		if (err){
+			throw err;
+		}
+		
+		fs.createReadStream(file).pipe(unzip.Extract({ 
+			path: options.sitePath 
+		})).on('close', function(){
+			exportManager.exportSite(path.join(__dirname, "../public"), function(folder){			
+				res.status(200).json({status: "OK"});
+			});
+		});
+	})
+	
+});
+
+/* GET upload image. */
+router.get("/export", isAuthenticated, function(req, res, next) {
+	exportManager.exportSite(uuid.v4(), function(folder){
+		exportManager.generateZip(folder, function(exportFile){
+			var stat = fs.statSync(exportFile);
+
+			res.writeHead(200, {
+				"Content-Type": "application/zip",
+				"Content-Length": stat.size
+			});
+
+			var readStream = fs.createReadStream(exportFile);
+			readStream.pipe(res);
+			
+			//once the file is sent, send 200 and delete the file from the server
+			readStream.on("end", function ()
+			{
+				fs.remove(exportFile);
+				return res.status(200);
+			});
+		})
+	});
+});
+
+/* POST remove content repeatable. */
+router.post("/remove", isAuthenticated, function(req, res, next) {
+	dataManager.removeRepeatable(req.body.name, req.query.page, req.body.repeatIndex, function(err){
+		if(err) throw err;
+		res.status(200).json({status: "OK"});		
+	});
+});
+
+/* POST make content repeatable. */
+router.post("/editable", isAuthenticated, function(req, res, next) {	
+	var selector = req.body.selector;
+	var page = req.query.page;
+	
+	if(!selector){
+		next();
+		return;
+	}
+	
+	var filepath = path.join(options.sitePath, page);
+	dataManager.makeElemEditable(selector, filepath, function(err, name){
+		if (err){			
+			res.status(400).json(err);
+			return;			
+		}
+		res.status(200).json({name: name});
+	});	
+});
+
+/* POST remove content repeatable. */
+router.delete("/editable/:id", isAuthenticated, function(req, res, next) {
+	
+	var id = req.params.id;
+	var page = req.query.page;
+	
+	if(!id){
+		next();
+		return;
+	}
+	
+	var filepath = path.join(options.sitePath, page);
+	dataManager.destroyElemEditable(id, filepath, function(err, name){
+		if (err) throw err;
+		res.status(200).json({name: name});
+	});	
+});
+
+module.exports = function(opt){
+	options = opt;
+	
+	dataManager = require("../managers/data_manager")(options.dataDb);
+	userManager = require("../managers/user_manager")(options.userDb);
+	exportManager = exporter(options);
+
+	return router;
+};
